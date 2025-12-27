f8ff2dbcbc4fd70126f3e2ca2a389c037f5d382d
Your Name <mailsonccardoso@gmail.com>
2025-12-26 23:47:23 -0300

    correção de ip

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://192.168.176.30:8000',
         changeOrigin: true,
         secure: false,
       }
