0ac2c4dfe2b27e53b172ee21e08e4cf550b199c0
Mailson <mailsonccardoso@gmail.com>
2026-06-08 14:29:31 -0300

    remove CORS middleware (Apache gerencia) e cria tabela global_settings

--- back/app/Http/Middleware/CorsMiddleware.php
+++ back/app/Http/Middleware/CorsMiddleware.php
@@ -12,12 +12,20 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        // Remove any existing CORS headers from server to avoid duplicates
+        $response->headers->remove('Access-Control-Allow-Origin');
+        $response->headers->remove('Access-Control-Allow-Methods');
+        $response->headers->remove('Access-Control-Allow-Headers');
+
+        // Set our own CORS headers
+        $origin = $request->header('Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', $origin);
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
 
         if ($request->isMethod('OPTIONS')) {
-            return response('', 200, $response->headers->all());
+            return response('', 204, $response->headers->all());
         }
 
         return $response;
