9d7395868f206d9be6a69c863d5c8b9544d53bee
Mailson <mailsonccardoso@gmail.com>
2026-06-08 14:38:15 -0300

    remove CORS duplicado do index.php e volta chamada direta à API

--- back/public/index.php
+++ back/public/index.php
@@ -3,16 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
-header('Access-Control-Allow-Headers: *');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
